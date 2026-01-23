170ccf3dcab31c8b3275a0accb4fdab8f7343772
hamitkardic <161644946+hamitkardic@users.noreply.github.com>
2026-01-23 17:10:46 +0100

    Initial Project Setup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import com.library.repository.UserDAO;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Welcome to the Library");
+        System.out.println("Welcome to the Library");
+        System.out.println("Please login.");
 
-        System.out.println( "Please login.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Username: ");
+        String inputUsername = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String inputPassword = scanner.nextLine();
+
+        UserDAO userDAO = new UserDAO();
+
+        if (userDAO.login(inputUsername, inputPassword)) {
+            System.out.println("Login successful");
+        } else {
+            System.out.println("Invalid credentials");
         }
+
+        scanner.close();
     }
+}
--- src/com/library/repository/UserDAO.java
+++ src/com/library/repository/UserDAO.java
@@ -1,4 +1,14 @@
 package com.library.repository;
 
+import com.library.model.User;
+
 public class UserDAO {
+
+    private static final User ADMIN =
+            new User("admin", "1234");
+
+    public boolean login(String username, String password) {
+        return ADMIN.getUsername().equals(username)
+                && ADMIN.getPassword().equals(password);
+    }
 }
