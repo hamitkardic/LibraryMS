bd6e86a89f23e122ba00f195d6624da33dc098cc
hamitkardic <161644946+hamitkardic@users.noreply.github.com>
2026-02-01 16:42:24 +0100

    Initial Project Setup

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,59 @@
 import com.library.repository.UserDAO;
-import java.io.Console;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to the Library");
-        System.out.println("Please login.");
 
-        Console console = System.console();
+        SwingUtilities.invokeLater(Main::createLoginUI);
 
-        if (console == null) {
-            System.out.println("No console available. Run from a terminal.");
-            return;
-        }
+    }
 
-        String username = console.readLine("Username: ");
-        char[] passwordChars = console.readPassword("Password: ");
-        String password = new String(passwordChars);
+    private static void createLoginUI(){
+        JFrame frame = new JFrame("Library Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 200);
+        frame.setLocationRelativeTo(null);
 
-        UserDAO userDAO = new UserDAO();
+        JPanel panel = new JPanel(new GridLayout(3, 2, 10, 10));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        if (userDAO.login(username, password)) {
-            System.out.println("Login successful");
-        } else {
-            System.out.println("Invalid credentials");
-        }
+        JLabel userLabel = new JLabel("Username:");
+        JTextField usernameField = new JTextField();
+
+        JLabel passLabel = new JLabel("Password:");
+        JPasswordField passwordField = new JPasswordField();
+
+        JButton loginButton = new JButton("Login");
+
+        panel.add(userLabel);
+        panel.add(usernameField);
+        panel.add(passLabel);
+        panel.add(passwordField);
+        panel.add(new JLabel());
+        panel.add(loginButton);
+
+        frame.add(panel);
+        frame.setVisible(true);
+
+        loginButton.addActionListener(e -> {
+            String username = usernameField.getText();
+            String password = new String(passwordField.getPassword());
+
+            UserDAO UserDAO = new UserDAO();
+
+            if(UserDAO.login(username, password)) {
+                JOptionPane.showMessageDialog(frame, "Login successful!");
+                frame.dispose();
+            } else {
+                JOptionPane.showMessageDialog(frame, "Invalid credentials", "Login failed", JOptionPane.ERROR_MESSAGE);
+                passwordField.setText("");
+            }
+
+        });
+
+
     }
 }
